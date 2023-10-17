45ef4006be4d6ab2d3ce705cd605efd500d01bad
pawan <patpawa3@gmail.com>
2023-10-17 18:01:55 +0530

    start01

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/get")
 	public String meth01() {
-		return "The first CI CD deployment pipeline";
+		return "New Version 01";
 	}
 
 }
