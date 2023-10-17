19b5558f4fac6536590fbef5f2948d074987ca8b
pawan <patpawa3@gmail.com>
2023-10-17 12:42:33 +0530

    code

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	@GetMapping("/get")
+	public String meth01() {
+		return "hello";
+	}
+
+}
