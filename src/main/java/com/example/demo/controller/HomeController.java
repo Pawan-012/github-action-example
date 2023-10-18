b93bb4478a3fac53b03031dbbd19083c1e90592c
pawan <patpawa3@gmail.com>
2023-10-18 17:38:51 +0530

    jk

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 	@GetMapping("/get")
 	public String meth01() {
-		return "Welcome to algo trading software";
+		return "!!!Welcome to algo trading software!!!";
 	}
 
 }
